bd1fc3eb971944b41ff3ee382f8beec4a21daa65
mohamedelalfy <39034135+mohamedelalfy@users.noreply.github.com>
2021-10-27 21:17:34 +0200

    backend

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
-const authJwt = require('./helpers/jwt');
-const errorHandler = require('./helpers/error-handler');
+// const authJwt = require('./helpers/jwt');
+// const errorHandler = require('./helpers/error-handler');
 
 app.use(cors());
-app.options('*', cors());
+// app.options('*', cors());
 
 //middleware
 app.use(express.json());
-app.use(morgan('tiny'));
-app.use(authJwt());
-app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
-app.use(errorHandler);
+// app.use(morgan('tiny'));
+// app.use(authJwt());
+// app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+// app.use(errorHandler);
 
 //Routes
 const categoriesRoutes = require('./routes/categories');
@@ -23,12 +23,12 @@ const productsRoutes = require('./routes/products');
 const usersRoutes = require('./routes/users');
 const ordersRoutes = require('./routes/orders');
 
-const api = process.env.API_URL;
+// const api = process.env.API_URL;
 
-app.use(`${api}/categories`, categoriesRoutes);
-app.use(`${api}/products`, productsRoutes);
-app.use(`${api}/users`, usersRoutes);
-app.use(`${api}/orders`, ordersRoutes);
+// app.use(`${api}/categories`, categoriesRoutes);
+// app.use(`${api}/products`, productsRoutes);
+app.use(`/users`, usersRoutes);
+// app.use(`${api}/orders`, ordersRoutes);
 
 //Database
 mongoose
@@ -47,5 +47,5 @@ mongoose
 const PORT = process.env.PORT || 3000;
 //Server
 app.listen(PORT, () => {
-    console.log('server is running http://localhost:3000');
+    console.log('http://localhost:3000');
 });
